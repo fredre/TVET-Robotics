57c29eb2ba096dac04883571c379b1808a593f11
Mighty Minds <36125129+MightyMinds@users.noreply.github.com>
2022-12-09 09:31:38 +0200

    Add files via upload

--- PiBot/L298N H-Bridge/H-bridge direction control.py
+++ PiBot/L298N H-Bridge/H-bridge direction control.py
@@ -1,32 +1,105 @@
+import LCD1602
 import RPi.GPIO as GPIO          
 from time import sleep
 
-in1 = 24
-in2 = 23
-en = 25
+LCD1602.init(0x27,1)
+
+in1 = 14
+in2 = 15
+in3 = 23
+in4 = 24
+enA = 18
+enB = 25
 temp1=1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(enA,GPIO.OUT)
+GPIO.setup(enB,GPIO.OUT)
+
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+pA=GPIO.PWM(enA,1000)
+pB=GPIO.PWM(enB,1000)
 
-def run():
+def fwd():
+    LCD1602.clear()
+    LCD1602.write(2,0, 'Forward')
+    pA.ChangeDutyCycle(50)
+    pB.ChangeDutyCycle(50)
     GPIO.output(in1,GPIO.HIGH)
     GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.HIGH)
+    GPIO.output(in4,GPIO.LOW)
 
-p.start(25)
+def rev():
+    LCD1602.clear()
+    LCD1602.write(2,0, 'Reverse')
+    pA.ChangeDutyCycle(50)
+    pB.ChangeDutyCycle(50)
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.HIGH)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.HIGH)
+
+def left_turn():
+    LCD1602.clear()
+    LCD1602.write(2,0, 'Left Turn')
+    pA.ChangeDutyCycle(30)
+    pB.ChangeDutyCycle(30)
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.HIGH)
+    GPIO.output(in3,GPIO.HIGH)
+    GPIO.output(in4,GPIO.LOW)
+    
+def right_turn():
+    LCD1602.clear()
+    LCD1602.write(2,0, 'Right Turn')
+    pA.ChangeDutyCycle(30)
+    pB.ChangeDutyCycle(30)
+    GPIO.output(in1,GPIO.HIGH)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.HIGH)
+
+def stop():
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.LOW)
+    LCD1602.clear()
+    LCD1602.write(4,0, 'Stop')
+                
+pA.start(25)
+pB.start(25)
+
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
 
-while(1):
-    run()
+try:
+    while(1):
+        fwd()
+        sleep(1)
+        left_turn()
+        sleep(1)
+        rev()
+        sleep(1)
+        right_turn()
+        sleep(1)
+        stop()
+        sleep(2)
+        
 
-    
-    
-    
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    LCD1602.clear()
+    sleep(0.2)
+    LCD1602.write(3,0,"Program End")
+    print('Program stopped')
+        
--- PiBot/L298N H-Bridge/H-bridge.py
+++ PiBot/L298N H-Bridge/H-bridge.py
@@ -1,53 +1,78 @@
 import RPi.GPIO as GPIO          
 from time import sleep
+#Motor A
+in1 = 14
+in2 = 15
+enA = 18
 
-in1 = 24
-in2 = 23
-en = 25
+#Motor B
+in3 = 23
+in4 = 24
+enB = 25
 temp1=1
 
+#Set Modes for GPIO Pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
+GPIO.setup(enA,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(enB,GPIO.OUT)
+
+#Initial status - Motors off
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
 
-p.start(25)
+#Motors Initial Frequency
+pA=GPIO.PWM(enA,1000)
+pB=GPIO.PWM(enB,1000)
+
+#Motor Initial speed 
+pA.start(25)
+pB.start(25)
+
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
 
 while(1):
-
     x=input("Command:")
-    
+   
     if x=='r':
         print("run")
         if(temp1==1):
          GPIO.output(in1,GPIO.HIGH)
          GPIO.output(in2,GPIO.LOW)
+         GPIO.output(in3,GPIO.HIGH)
+         GPIO.output(in4,GPIO.LOW)
          print("forward")
          x='z'
         else:
          GPIO.output(in1,GPIO.LOW)
          GPIO.output(in2,GPIO.HIGH)
+         GPIO.output(in3,GPIO.LOW)
+         GPIO.output(in4,GPIO.HIGH)
          print("backward")
          x='z'
-
 
     elif x=='s':
         print("stop")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.LOW)
         x='z'
 
     elif x=='f':
         print("forward")
         GPIO.output(in1,GPIO.HIGH)
         GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in4,GPIO.LOW)
         temp1=1
         x='z'
 
@@ -55,22 +80,27 @@ while(1):
         print("backward")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.HIGH)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.HIGH)
         temp1=0
         x='z'
 
     elif x=='l':
         print("low")
-        p.ChangeDutyCycle(25)
+        pA.ChangeDutyCycle(25)
+        pB.ChangeDutyCycle(25)
         x='z'
 
     elif x=='m':
         print("medium")
-        p.ChangeDutyCycle(50)
+        pA.ChangeDutyCycle(50)
+        pB.ChangeDutyCycle(50)
         x='z'
 
     elif x=='h':
         print("high")
-        p.ChangeDutyCycle(100)
+        pA.ChangeDutyCycle(75)
+        pB.ChangeDutyCycle(75)
         x='z'
      
     
